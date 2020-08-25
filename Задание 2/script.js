b826f09cbabaa77bc26b62fed2e57194569e250a
Анастасия <64712603+Nasty-aa@users.noreply.github.com>
2020-08-25 22:06:00 +0300

    Add files via upload

---
+++ Задание 2/script.js
@@ -0,0 +1,8 @@
+//a) Объявить две переменные: admin и name. Записать в name строку «Василий»;
+let name;
+let admin; 
+name = 'Василий';
+
+//b) Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
+admin = name;
+alert(admin);
