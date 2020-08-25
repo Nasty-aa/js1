79b0a4d93c478014f3cb727f07140ae6f210c4aa
Анастасия <64712603+Nasty-aa@users.noreply.github.com>
2020-08-25 22:03:38 +0300

    Delete script.js

--- Задание 2/script.js
+++
@@ -1,8 +0,0 @@
-//a) Объявить две переменные: admin и name. Записать в name строку «Василий»;
-let name;
-let admin; 
-name = 'Василий';
-
-//b) Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
-admin = name;
-alert(admin);
